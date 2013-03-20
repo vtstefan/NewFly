3b83e5574878e15f72d6317b45e5ff3a6beaab9a
Stefan Bayer <stefan.bayer@versatec.de>
2013-03-20 15:35:40 +0100

    added unit testing

---
+++ NewFlyLogicTests/AppDelegateTest.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegateTest.h
+//  NewFly
+//
+//  Created by Stefan Bayer on 20.03.13.
+//  Copyright (c) 2013 Versatec GmbH. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppDelegateTest : SenTestCase
+
+@end
---
+++ NewFlyLogicTests/AppDelegateTest.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegateTest.m
+//  NewFly
+//
+//  Created by Stefan Bayer on 20.03.13.
+//  Copyright (c) 2013 Versatec GmbH. All rights reserved.
+//
+
+#import "AppDelegateTest.h"
+
+@implementation AppDelegateTest
+
+- (void)testLaunch
+{
+    STAssertTrue(true, nil);
+}
+
+@end
